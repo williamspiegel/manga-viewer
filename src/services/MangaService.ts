765d44b3d5011d884341fcef942295a2066b9800
William Spiegel <willy.spiegel@gmail.com>
2022-11-06 17:05:56 -0500

    barebones MangaDex implementation (untested)

--- src/services/MangaService.ts
+++ src/services/MangaService.ts
@@ -1,12 +1,12 @@
-type MangaEntry = {
-  //ID that goes into getMangaCover
+export type MangaEntry = {
+  // ID that goes into getMangaCover
   id: string;
 
-  //Title of manga (may vary depending on user's locale)
+  // Title of manga (may vary depending on user's locale)
   title: string;
 
-  //URL that points to cover image of manga (some services hide this in a separate service)
-  coverURL: Promise<string>;
+  // URL for cover image of manga
+  coverURL: string;
 };
 
 export interface MangaService {
---
+++ src/services/sites/MangaDex/MangaDex.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { MangaService } from '../../mangaService';
+import MangaDexMockData from './MangaDexMockData.json';
+
+const BASE_URL = 'api.mangadex.org';
+const BASE_URL_CDN = 'uploads.mangadex.org';
+export class MangaDex implements MangaService {
+  async getMangaList(query: string) {
+    const config = {
+      method: 'get',
+      url: `${BASE_URL}/manga?title=Berserk&includes[]=cover_art&includes[]=chapter`,
+    };
+
+    try {
+      const response = await axios(config);
+      const { data }: typeof MangaDexMockData = response.data;
+      return data?.map((curr) => ({
+        id: curr?.id,
+        //TODO: dynamically localize title
+        title: curr?.attributes?.title['en'],
+        coverURL: `${BASE_URL_CDN}/${curr?.id}/${curr?.relationships?.find(
+          (relationship: any) => relationship.type === 'cover_art'
+        )}`,
+      }));
+    } catch (error) {
+      console.log(error);
+      return [{ id: '', title: '', coverURL: '' }];
+    }
+  }
+}
