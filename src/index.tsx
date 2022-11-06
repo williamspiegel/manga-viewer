f53f2b246eb54f2628b4e46a4907568315bb842c
William Spiegel <willy.spiegel@gmail.com>
2022-11-06 13:45:44 -0500

    correct broken translations implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { store } from './app/store';
+import './i18n/config';
 import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
