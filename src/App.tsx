712886dd6ff5d204d7968db504591dd647a13112
William Spiegel <willy.spiegel@gmail.com>
2022-11-06 14:00:04 -0500

    remove unused line

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useTranslation } from 'react-i18next';
 import './App.css';
 import Routing from './Routing';
 function App() {
-  const { t } = useTranslation();
   return (
     <div className='App'>
       <header className='App-header'>
