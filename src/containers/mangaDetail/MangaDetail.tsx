dd6a1c1684e9b7cbe6e492a6b302f1be271fab54
William Spiegel <willy.spiegel@gmail.com>
2022-11-10 18:36:06 -0500

    fix styling for mangadetail

--- src/containers/mangaDetail/MangaDetail.tsx
+++ src/containers/mangaDetail/MangaDetail.tsx
@@ -9,10 +9,21 @@ const MangaDetail = (props: PropTypes) => {
     (state: any) => state.mangaDetail.mangaEntry
   );
   return (
-    <div className='p-1'>
-      <img src={coverURL} alt={title} id={id} className='rounded-md w-1/2' />
-      <text>{title}</text>
-    </div>
+    <>
+      <div className='flex flex-row'>
+        <img
+          src={coverURL}
+          alt={title}
+          id={id}
+          className='basis-1/3 w-1/3 object-contain'
+        />
+        <div className='basis-2/3 p-5 flex flex-col'>
+          <text className=' text-center'>{title}</text>
+          <text>{description}</text>
+        </div>
+      </div>
+      <div className='divider'></div>
+    </>
   );
 };
 
