fc4f18f44be24b1e7a302f1ee368ba802a9a7977
William Spiegel <willy.spiegel@gmail.com>
2022-11-15 20:33:28 -0500

    add initial carousel behavior

--- src/containers/mangaViewer/MangaViewer.tsx
+++ src/containers/mangaViewer/MangaViewer.tsx
@@ -1,23 +1,28 @@
 import { useQuery } from '@tanstack/react-query';
 import { useSelector } from 'react-redux';
-import { Carousel } from 'react-responsive-carousel';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { MangaDex } from '../../services/sites/MangaDex/MangaDex';
-
+// Import Swiper styles
+import 'swiper/css';
 type Props = {};
 
 export default function MangaViewer({}: Props) {
   const chapterId = useSelector((state: any) => state.mangaViewer.chapterId);
 
-  const { data } = useQuery({
-    queryKey: [],
+  const { data, isLoading } = useQuery({
+    queryKey: [chapterId],
     queryFn: () => MangaDex.getPages(chapterId),
   });
 
-  return (
-    <Carousel dynamicHeight>
+  return isLoading ? (
+    <></>
+  ) : (
+    <Swiper className='h-1/2'>
       {data?.map((curr) => (
-        <img src={curr} alt={curr} />
+        <SwiperSlide className='object-contain'>
+          <img src={curr} alt={curr} />
+        </SwiperSlide>
       ))}
-    </Carousel>
+    </Swiper>
   );
 }
