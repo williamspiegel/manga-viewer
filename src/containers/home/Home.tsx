ab4fdeb1a64db3a0cf09c9c265a1b1bbd2f3cea6
William Spiegel <willy.spiegel@gmail.com>
2022-11-19 22:53:45 -0500

    temporarily check if Home can go through CORS

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,6 +1,26 @@
-import { useTranslation } from 'react-i18next';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import MangaCovers from '../../components/MangaCovers';
+import { MangaDex } from '../../services/sites/MangaDex/MangaDex';
 
 export default function Home() {
-  const { t } = useTranslation();
-  return <div>{t('Home')}</div>;
+  const [input, setInput] = useState('');
+  const { data } = useQuery({
+    queryKey: [input],
+    queryFn: () => MangaDex.getMangaList(input),
+  });
+
+  return (
+    <div className=' flex flex-col  items-center'>
+      <input
+        type='text'
+        placeholder='Type here'
+        className='input input-bordered w-full max-w-xs m-3 flex'
+        onInput={(event) => {
+          setInput(event.currentTarget.value);
+        }}
+      />
+      <MangaCovers mangaEntries={data || []} />
+    </div>
+  );
 }
