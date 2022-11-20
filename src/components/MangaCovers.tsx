81870249e0db6b4b8d2ae6f7accac7690ec9b317
William Spiegel <willy.spiegel@gmail.com>
2022-11-19 20:19:25 -0500

    refactor MangaCovers to use Swiper and clamp title to 2 lines

--- src/components/MangaCovers.tsx
+++ src/components/MangaCovers.tsx
@@ -1,7 +1,7 @@
 import { Link } from '@tanstack/react-location';
 import { useDispatch } from 'react-redux';
-import AutoSizer from 'react-virtualized-auto-sizer';
-import { VariableSizeList as List } from 'react-window';
+import { Navigation, Virtual } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { setMangaEntry } from '../containers/mangaDetail/mangaDetailSlice';
 import { MangaEntry } from '../services/MangaService';
 
@@ -9,45 +9,44 @@ type PropTypes = { mangaEntries: MangaEntry[]; className?: string | undefined };
 
 const MangaCovers = ({ mangaEntries, className = '' }: PropTypes) => {
   const dispatch = useDispatch();
-  const Column = ({ index, style }: { index: number; style: Object }) => {
-    const { coverURL, title, id, description, author } = mangaEntries[index];
 
-    return (
-      <Link
-        to='/mangaDetail'
-        onClick={() => {
-          dispatch(setMangaEntry({ coverURL, title, id, description, author }));
-        }}
-      >
-        <div className='p-1' key={index} data-index={index} style={style}>
-          <img
-            src={coverURL}
-            alt={title}
-            id={id}
-            className='rounded-md object-cover aspect-[2/3]'
-          />
-          <text>{title}</text>
-        </div>
-      </Link>
-    );
-  };
   return (
-    // TODO: figure out why w-full does not work for className
-    /* TODO: test out setting height and itemSize based off only the height 
-    of the initial render to avoid virtualization jank with new items being loaded in */
-    <AutoSizer className={'flex-1 mt-5' + className} style={{ width: '100%' }}>
-      {({ width }) => (
-        <List
-          height={180}
-          width={width}
-          itemCount={mangaEntries.length}
-          layout='horizontal'
-          itemSize={() => 100}
-        >
-          {Column}
-        </List>
-      )}
-    </AutoSizer>
+    <Swiper
+      slidesPerView={3}
+      centeredSlides={true}
+      spaceBetween={10}
+      navigation={true}
+      virtual
+      className='w-full'
+      modules={[Virtual, Navigation]}
+    >
+      {mangaEntries.map((slideContent, index) => {
+        const { coverURL, title, id, description, author } = slideContent;
+
+        return (
+          <SwiperSlide key={index} virtualIndex={index}>
+            <Link
+              to='/mangaDetail'
+              onClick={() => {
+                dispatch(
+                  setMangaEntry({ coverURL, title, id, description, author })
+                );
+              }}
+            >
+              <div className='p-1'>
+                <img
+                  src={coverURL}
+                  alt={title}
+                  id={id}
+                  className='rounded-md object-cover aspect-[2/3]'
+                />
+                <p className='line-clamp-2'>{title}</p>
+              </div>
+            </Link>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
