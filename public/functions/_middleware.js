b3c8bd0364294b483e4a7d86f39187f254f44b13
William Spiegel <willy.spiegel@gmail.com>
2022-11-19 23:15:52 -0500

    checking if Cloudflare recognizes middleware location

--- public/functions/_middleware.js
+++ public/functions/_middleware.js
@@ -15,6 +15,6 @@ export const onRequestOptions = async () => {
 export const onRequest = async ({ next }) => {
   const response = await next();
   response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
+  response.headers.set('Access-Control-Max-Age', '86401');
   return response;
 };
